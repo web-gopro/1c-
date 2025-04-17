d3412a2ae2a746174fe918a46553ad4276298017
Jasur <xaydarovjasur999@gmail.com>
2025-04-17 18:13:24 +0500

    cheacking

--- Program.cs
+++ Program.cs
@@ -1,2 +1,29 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿string txt = "The character \\ is called backslash.";
+Console.WriteLine(txt);
+
+int a=2;
+if (txt[0]!='T'){
+
+    Console.WriteLine("siz tugri yuldasiz");
+}
+else if (a==2){
+
+    Console.WriteLine("aaaaaaaaa");
+}
+else{
+
+Console.WriteLine("azizzzzzzzzzzzz");
+
+}
+
+
+
+int myAge =20;
+
+bool res=(myAge==21) ? true:false;
+
+double math =Math.Abs(-4.7);
+Console.WriteLine(res);
+
+Console.WriteLine(math);
+
